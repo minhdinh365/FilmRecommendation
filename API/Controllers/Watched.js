7d04c6e82a8645e38e73a71515f0f6d5273cd35e
minhdinh365 <minhdinhtran0@gmail.com>
2021-12-06 10:05:00 +0700

    push code

--- API/Controllers/Watched.js
+++ API/Controllers/Watched.js
@@ -1,20 +1,19 @@
 import { Watchedes } from "../models/Watched.js";
 
-
 export const getWatched = async (req, res) => {
   try {
-    const films = await Watchedes.find({ username: req.query.id })
-      .populate('infoFilm')
+    const films = await Watchedes.find({ username: req.query.id }).populate(
+      "infoFilm",
+      "-cast -keywords -crew -overview -backdrop_path -popularity -release_date -video -vote_count -revenue -video_id -tagline -run_time -budget -category -adult -original_language -original_title"
+    );
     if (films.length > 0) {
       res.json({
-        films: films
-      })
-    }
-    else {
+        films: films.reverse(),
+      });
+    } else {
       res.json({
-        films: false
-      })
-
+        films: false,
+      });
     }
   } catch (err) {
     return res.status(500).json({ msg: err.message });
@@ -22,21 +21,19 @@ export const getWatched = async (req, res) => {
 };
 export const postWatched = async (req, res) => {
   try {
-    const film = req.body
-    const finds = await Watchedes.find({ id: film.id })
+    const film = req.body;
+    const finds = await Watchedes.find({ id: film.id });
     if (finds.length > 0) {
-      res.json('Phim nay xem roi')
-    }
-    else {
+      res.json("Phim nay xem roi");
+    } else {
       let phim = new Watchedes({
         id: film.id,
         username: film.username,
-        watched: film.watched
-      })
-      phim.save()
+        watched: film.watched,
+      });
+      phim.save();
     }
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
 };
-
