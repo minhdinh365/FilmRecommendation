42ed5029e50a55e7b6359bef724dc67976af20f3
minhdinh365 <minhdinhtran0@gmail.com>
2021-12-13 08:49:44 +0700

    fix bug

---
+++ RecommendationAPI/TrainAPI/Models/FilmsModel.cs
@@ -0,0 +1,55 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace TrainAPI.Models
+{
+    public class FilmsModel
+    {
+        public FilmsModel(int id, string username, string title, float evaluate, string poster_path, int run_time, string release_date)
+        {
+            this.id = id;
+            this.title = title;
+            this.username = username;
+            this.evaluate = evaluate;
+            this.poster_path = poster_path;
+            this.run_time = run_time;
+            this.release_date = release_date;
+
+        }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public int run_time { get; set; }
+
+        public string release_date { get; set; }
+
+        public string username { get; set; }
+
+        public float evaluate { get; set; }
+        public string poster_path { get; set; }
+    }
+
+    public class ListFilm
+    {
+        public ListFilm(string json)
+        {
+            films = new List<FilmsModel>();
+            dynamic myObject = JValue.Parse(json);
+            foreach (dynamic film in myObject.List)
+            {
+                films.Add(new FilmsModel((int)film.id_film, (string)film.id_info, (string)film.film.title, (float)film.evaluate, (string)film.film.poster_path, (int)film.film.run_time, (string)film.film.release_date));
+            }
+        }
+        
+        public List<FilmsModel> films { get; set; }
+    }
+
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
---
+++ RecommendationAPI/TrainAPI/Models/UsersModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainAPI.Models
+{
+    public class UsersModel
+    {
+        public int id_film { get; set; }
+
+        public string id_info { get; set; }
+
+        public int evaluate { get; set; }
+    }
+}
